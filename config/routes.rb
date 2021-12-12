7a37d247e3e7dada0fe52417486293329903e95e
Susan Li <susanli@susans-mbp.dyn.brandeis.edu>
2021-12-12 12:17:41 -0500

    Handle invalid routes for specific resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get 'do_search'
       post 'cancel', to: 'events#cancel'
       post 'update_calendar', to: 'events#update_calendar'
+      get ':not_found' => redirect('/'), :constraints => { :not_found => /.*/ }
     end
   end
   resources :participants do
@@ -14,6 +15,7 @@ Rails.application.routes.draw do
       post   '/login',   to: 'participant_sessions#create'
       post 'add', to: 'participants#add_to_schedule'
       post 'remove', to: 'participants#remove_from_schedule'
+      get ':not_found' => redirect('/'), :constraints => { :not_found => /.*/ }
     end
   end
   resources :organizers do
@@ -21,6 +23,7 @@ Rails.application.routes.draw do
       get  '/signup',  to: 'organizers#new'
       get    '/login',   to: 'organizer_sessions#new'
       post   '/login',   to: 'organizer_sessions#create'
+      get ':not_found' => redirect('/'), :constraints => { :not_found => /.*/ }
     end
   end
 
@@ -30,6 +33,7 @@ Rails.application.routes.draw do
       get 'do_search'
       get 'search_available'
       get 'do_search_available'
+      get ':not_found' => redirect('/'), :constraints => { :not_found => /.*/ }
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
